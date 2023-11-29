91de28eb186736909eb3563954630f7d422771bf
v_xmjjxwu <769524158@qq.com>
2023-11-29 20:15:28 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import './styles/index.scss'
 
 library.add(fas)
 
-
-
 export { default as Button } from './components/Button'
 export { default as Menu } from './components/Menu'
 export { default as AutoComplete } from './components/AutoComplete'
