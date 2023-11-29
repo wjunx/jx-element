b9915c7e96985bad706668f170a02fd2c8ed9ee7
v_xmjjxwu <769524158@qq.com>
2023-11-29 19:26:46 +0800

    feat: 自己的组件库

---
+++ jest.test.js
@@ -0,0 +1,4 @@
+test('jest test', () => {
+  
+  expect(2 + 2).toBe(4)
+});
---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Button from './components/Button/button'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Button btnType='primary'>btn</Button>
+    </div>
+  );
+}
+
+export default App;
